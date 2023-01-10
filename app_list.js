b0ccbe1b065534f78fd0d6cf4e328d17215900f3
saanghyuk <saanghyuk@gmail.com>
2023-01-10 23:56:38 +0900

    after 20230110 review

---
+++ app_list.js
@@ -0,0 +1,72 @@
+const { request } = require("express");
+const express = require("express");
+let members = require("./members");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/members", (req, res) => {
+  let { team } = req.query;
+  if (team) {
+    const teamMembers = members.filter(
+      m => m.team.toLowerCase() == team.toLowerCase()
+    );
+    res.send(teamMembers);
+  } else {
+    res.send(members);
+  }
+});
+
+app.get("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const member = members.find(m => m.id == Number(id));
+
+  if (member) {
+    res.send(member);
+  } else {
+    res.status(404).send({ message: "There is no such member" });
+  }
+});
+app.post("/api/members", (req, res) => {
+  const newMember = req.body;
+  members.push(newMember);
+  res.send(newMember);
+});
+
+app.put("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const newInfo = req.body;
+  const member = members.find(m => m.id === Number(id));
+  if (member) {
+    Object.keys(newInfo).forEach(prop => {
+      member[prop] = newInfo[prop];
+    });
+    res.send(member);
+  } else {
+    res.status(404).send({ message: "There is no member with the id" });
+  }
+});
+
+app.delete("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+
+  const membersCount = members.length;
+
+  members = members.filter(member => member.id !== Number(id));
+
+  if (members.length <= membersCount) {
+    res.send({ message: "Deleted" });
+  } else {
+    res.status(404).send({ message: "There is no member with the id" });
+    f;
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening...");
+});
